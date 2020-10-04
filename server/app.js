8dd30098223d8791835514873e57d22be0b939e9
lmcuongdev <cuong2000hy@gmail.com>
2020-10-04 13:53:05 +0700

    refactor + add nodemon

--- server/app.js
+++ server/app.js
@@ -1,23 +1,30 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const config = require("./config");
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const postRouter = require('./api/modules/post/post.router');
-const userRouter = require('./api/modules/user/user.router');
+const postRouter = require("./api/modules/post/post.router");
+const userRouter = require("./api/modules/user/user.router");
 
-mongoose.connect(config.mongoConnectionString);
-const PORT =process.env.PORT || 6969;
+mongoose
+  .connect(config.mongoConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Successfully connected to database"))
+  .catch(() => console.log("Error connecting to database"));
+const PORT = process.env.PORT || 6969;
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/', express.static('../client'));
-app.use('/api/post', postRouter);
-app.use('/api/user', userRouter);
 
-app.listen(PORT, function() {
+app.use("/", express.static("../client"));
+app.use("/api/post", postRouter);
+app.use("/api/user", userRouter);
+
+app.listen(PORT, function () {
   console.log(`Server is listening on ${PORT}`);
 });
-
