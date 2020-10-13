83c4535501fc1b78c526818de0e48c4180379944
lmcuongdev <cuong2000hy@gmail.com>
2020-10-13 20:49:13 +0700

    change db link

--- server/config/index.js
+++ server/config/index.js
@@ -2,8 +2,8 @@ const { DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
 module.exports = {
   // mongoConnectionString: 'mongodb://localhost:27017/testDb',
   // mongoConnectionString:
-  // "mongodb+srv://admin:chabaa20@cluster0-pqzf1.mongodb.net/travel?retryWrites=true&w=majority",
-  mongoConnectionString: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.8snuq.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+  "mongodb+srv://admin:chabaa20@cluster0-pqzf1.mongodb.net/travel?retryWrites=true&w=majority",
+  // mongoConnectionString: `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.8snuq.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
   salt: 3,
   privateKey: "navi",
 };
